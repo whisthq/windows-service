ca8ac3bc1c8b735f85042deb96f0194b21baccd8
Philippe Noël <philippe_noel@college.harvard.edu>
2020-06-10 22:35:56 -0400

    fractal emoji

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 aws s3 cp FractalService/FractalService/bin/Debug/FractalService.exe s3://fractal-cloud-setup-s3bucket/FractalService.exe && \
-curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"fractal-bot\", \"text\": \"FractalService.exe updated in AWS S3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"fractal-bot\", \"text\": \"FractalService.exe updated in AWS S3\", \"icon_emoji\": \":fractal:\"}" https://hooks.slack.com/services/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
