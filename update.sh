63a3008469aa340e274e504b7c9f8234bf643f94
Philippe Noël <philippe_noel@college.harvard.edu>
2020-06-11 10:09:47 -0400

    upload to specific branches on the S3 bucket

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-aws s3 cp FractalService/FractalService/bin/Debug/FractalService.exe s3://fractal-cloud-setup-s3bucket/FractalService.exe && \
+aws s3 cp FractalService/FractalService/bin/Debug/FractalService.exe s3://fractal-cloud-setup-s3bucket/master/FractalService.exe && \
+aws s3 cp FractalService/FractalService/bin/Debug/FractalService.exe s3://fractal-cloud-setup-s3bucket/dev/FractalService.exe && \
+aws s3 cp FractalService/FractalService/bin/Debug/FractalService.exe s3://fractal-cloud-setup-s3bucket/staging/FractalService.exe && \
 curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"fractal-bot\", \"text\": \"FractalService.exe updated in AWS S3\", \"icon_emoji\": \":fractal:\"}" https://hooks.slack.com/services/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
