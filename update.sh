301fc0afcbb24fadd52dbb21da42e5a7d137932d
philippemnoel <philippe_noel@college.harvard.edu>
2020-06-11 22:01:58 -0400

    fix path for update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws s3 cp FractalService/FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/master/FractalService.exe && \
-aws s3 cp FractalService/FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/dev/FractalService.exe && \
-aws s3 cp FractalService/FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/staging/FractalService.exe && \
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/master/FractalService.exe && \
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/dev/FractalService.exe && \
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/staging/FractalService.exe && \
 curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"fractal-bot\", \"text\": \"FractalService.exe updated in AWS S3\", \"icon_emoji\": \":fractal:\"}" https://hooks.slack.com/services/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
