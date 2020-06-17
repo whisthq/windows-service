4226455c512912cfd6391039772174f64cbcf8cf
Philippe Noël <phil@fractalcomputers.com>
2020-06-17 11:21:27 -0400

    update to branch/Windows path in S3

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/master/FractalService.exe && \
-aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/dev/FractalService.exe && \
-aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/staging/FractalService.exe && \
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/master/Windows/FractalService.exe && \
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/dev/Windows/FractalService.exe && \
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/staging/Windows/FractalService.exe && \
 curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"fractal-bot\", \"text\": \"FractalService.exe updated in AWS S3\", \"icon_emoji\": \":fractal:\"}" https://hooks.slack.com/services/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
