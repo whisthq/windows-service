9a1c00248dc42cb0fa5edf2d21ff3f5a7dd1bfb2
Philippe Noël <phil@tryfractal.com>
2020-10-14 11:14:32 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/master/Windows/FractalService.exe && \
-aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/dev/Windows/FractalService.exe && \
-aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/staging/Windows/FractalService.exe && \
-curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"fractal-bot\", \"text\": \"FractalService.exe updated in AWS S3\", \"icon_emoji\": \":fractal:\"}" https://hooks.slack.com/services/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
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/master/Windows/FractalService.exe
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/dev/Windows/FractalService.exe
+aws s3 cp FractalService/bin/Release/FractalService.exe s3://fractal-cloud-setup-s3bucket/staging/Windows/FractalService.exe
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#alerts\", \"username\": \"Fractal Bot\", \"text\": \"Windows Service pushed to production via S3 upload.\", \"icon_emoji\": \":fractal:\"}" https://hooks.slack.com/services/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
