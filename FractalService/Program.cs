f6819789c5f1c620694c2975d91cf8a6dab36a89
Philippe <phil@fractalcomputers.com>
2020-10-28 00:52:10 -0400

    addedsentry

--- FractalService/Program.cs
+++ FractalService/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using Sentry;
 
 namespace FractalService
 {
@@ -15,14 +16,16 @@ namespace FractalService
         static void Main()
         {
             // Initialize Sentry
-            use __ = SentrySdk.Init("https://147514cde6004c8ca8171f1f37c2a919@o400459.ingest.sentry.io/5493264");
-
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            using (SentrySdk.Init("https://147514cde6004c8ca8171f1f37c2a919@o400459.ingest.sentry.io/5493264"))
             {
+                // App code
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
                 new FractalService()
-            };
-            ServiceBase.Run(ServicesToRun);
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
         }
     }
 }
